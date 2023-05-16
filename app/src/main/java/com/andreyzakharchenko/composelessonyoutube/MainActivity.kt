38581904ca965e30d44cc4b28d7775179b7368ee
azakharcenko <azakharcenko@wooppay.com>
2023-05-16 14:19:53 +0600

    - state

--- app/src/main/java/com/andreyzakharchenko/composelessonyoutube/MainActivity.kt
+++ app/src/main/java/com/andreyzakharchenko/composelessonyoutube/MainActivity.kt
@@ -1,44 +1,54 @@
 package com.andreyzakharchenko.composelessonyoutube
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column {
-                ListItem(name = "Тарантиныч", prof = "Director")
-                ListItem(name = "Тарантиныч", prof = "Director")
-                ListItem(name = "Тарантиныч", prof = "Director")
-                ListItem(name = "Тарантиныч", prof = "Director")
-                ListItem(name = "Тарантиныч", prof = "Director")
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircleItem()
             }
 
 
@@ -48,42 +58,28 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-private fun ListItem(name: String, prof: String) {
-    Card(
+private fun CircleItem() {
+    val counter = remember {
+        mutableStateOf(0)
+    }
+    val color = remember {
+        mutableStateOf(Color.Blue)
+    }
+
+    Box(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(12.dp),
-        shape = RoundedCornerShape(15.dp),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 10.dp
-        )
-    ) {
-        Box() {
-            Row(
-                modifier = Modifier
-                    .background(Color.Yellow)
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.man),
-                    contentDescription = "image",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .padding(6.dp)
-                        .size(64.dp)
-                        .clip(CircleShape)
-                )
-                Column(
-                    modifier = Modifier
-                        .background(Color.Red)
-                        .padding(start = 6.dp, end = 6.dp)
-                        .fillMaxWidth()
-                ) {
-                    Text(text = name)
-                    Text(text = prof)
+            .size(100.dp)
+            .background(color = color.value, shape = CircleShape)
+            .clickable {
+                when(++counter.value) {
+                    10 -> color.value = Color.Red
                 }
-            }
-        }
+            },
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = counter.value.toString(),
+            style = TextStyle(color = Color.White, fontSize = 20.sp)
+        )
     }
 }
