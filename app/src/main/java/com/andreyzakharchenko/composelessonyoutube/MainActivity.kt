f3312a026e303540bfc4991506955e890d736352
azakharcenko <azakharcenko@wooppay.com>
2023-05-16 11:26:14 +0600

    - Card

--- app/src/main/java/com/andreyzakharchenko/composelessonyoutube/MainActivity.kt
+++ app/src/main/java/com/andreyzakharchenko/composelessonyoutube/MainActivity.kt
@@ -3,57 +3,85 @@ package com.andreyzakharchenko.composelessonyoutube
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.andreyzakharchenko.composelessonyoutube.ui.theme.ComposeLessonYouTubeTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Column {
+                ListItem(name = "Тарантиныч", prof = "Director")
+                ListItem(name = "Тарантиныч", prof = "Director")
+                ListItem(name = "Тарантиныч", prof = "Director")
+                ListItem(name = "Тарантиныч", prof = "Director")
+                ListItem(name = "Тарантиныч", prof = "Director")
+            }
+
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        )
+    ) {
+        Box() {
             Row(
                 modifier = Modifier
-                    .background(Color.Gray)
-                    .fillMaxWidth()
-                    .fillMaxHeight(0.5f)
+                    .background(Color.Yellow)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
             ) {
+                Image(
+                    painter = painterResource(id = R.drawable.man),
+                    contentDescription = "image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(6.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
                 Column(
                     modifier = Modifier
-                        .background(Color.Green)
-                        .fillMaxHeight(0.85f)
-                        .fillMaxWidth(0.5f),
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                        .background(Color.Red)
+                        .padding(start = 6.dp, end = 6.dp)
+                        .fillMaxWidth()
                 ) {
-                    Text(text = "Hello")
-                    Text(text = "Madrid")
-                    Text(text = "Kaspa")
-                }
-
-                Column(
-                    modifier = Modifier
-                        .background(Color.Yellow)
-                        .fillMaxHeight(0.5f)
-                        .fillMaxWidth(),
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "Hello")
-                    Text(text = "Madrid")
-                    Text(text = "Kaspa")
+                    Text(text = name)
+                    Text(text = prof)
                 }
             }
         }
